45bef9f5bef55a7467020f8bc0eeab6c56d52782
vic1vega <janusz.pacholski@outlook.com>
2018-12-29 20:54:41 +0100

    improvement index site using bootstrap style

--- Models/MockItemRepository.cs
+++ Models/MockItemRepository.cs
@@ -20,9 +20,9 @@ namespace HomeLibrary.Models
         {
             items = new List<Item>
             {
-                new Item {AuthorName="Alejandro González Iñárritu",Cover= "https://www.filmweb.pl/video/zwiastun/nr+2+polski-37388",ItemId=1,RentDate=0,RentName=null,Title="Zjawa",Type=Types.DVD,UserId=0,Year=2015 },
+                new Item {AuthorName="Alejandro González Iñárritu",Cover= "https://ssl-gfx.filmweb.pl/po/65/83/586583/7749389.3.jpg",ItemId=1,RentDate=0,RentName=null,Title="Zjawa",Type=Types.DVD,UserId=0,Year=2015 },
                 new Item {AuthorName="Queen",Cover= "https://images-na.ssl-images-amazon.com/images/I/6139YfAAyBL._SS500.jpg",ItemId=2,RentDate=0,RentName=null,Title="Jazz",Type=Types.CD,UserId=0,Year=1978 },
-                new Item {AuthorName="George Orwell",Cover= "https://pl.wikipedia.org/wiki/Plik:Animal_Farm_-_1st_edition.jpg",ItemId=3,RentDate=0,RentName=null,Title="Folwark Zwierzęcy",Type=Types.Book,UserId=0,Year=1945 }
+                new Item {AuthorName="George Orwell",Cover= "https://upload.wikimedia.org/wikipedia/commons/f/fb/Animal_Farm_-_1st_edition.jpg",ItemId=3,RentDate=0,RentName=null,Title="Folwark Zwierzęcy",Type=Types.Book,UserId=0,Year=1945 }
             };
         }
 
