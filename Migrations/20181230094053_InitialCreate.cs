a012e08dc561e0499f89380ad4e8a002e54212e3
vic1vega <janusz.pacholski@outlook.com>
2018-12-30 10:44:35 +0100

    create and update first migrations for sqlite db

---
+++ Migrations/20181230094053_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HomeLibrary.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    ItemId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserId = table.Column<int>(nullable: false),
+                    Cover = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: true),
+                    Year = table.Column<int>(nullable: false),
+                    Type = table.Column<int>(nullable: false),
+                    AuthorName = table.Column<string>(nullable: true),
+                    RentDate = table.Column<int>(nullable: false),
+                    RentName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.ItemId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Items");
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -16,17 +16,18 @@ namespace HomeLibrary
     {
         public IConfiguration Configuration { get; }
 
-        public Startup (IConfiguration configuration)
+        public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-        
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultDatabase")));
-            services.AddTransient<IItemRepository,ItemRepository>();
+            services.AddDbContext<AppDbContext>
+                (options => options.UseSqlite("Data Source=HomeLibrary.db"));
+            services.AddTransient<IItemRepository, ItemRepository>();
             services.AddMvc();
         }
 
