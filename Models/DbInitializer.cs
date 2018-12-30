b05cc2ca7882898b15e33dfd481119d9da95d195
vic1vega <janusz.pacholski@outlook.com>
2018-12-30 11:24:15 +0100

    create seed data class in Model + initialize seed in Program Main Class

---
+++ Models/DbInitializer.cs
@@ -0,0 +1,20 @@
+using System.Linq;
+
+namespace HomeLibrary.Models
+{
+    public class DbInitializer
+    {
+        public static void Seed(AppDbContext context)
+        {
+            if (!context.Items.Any())
+            {
+                context.AddRange(
+                    new Item { AuthorName = "Alejandro González Iñárritu", Cover = "https://ssl-gfx.filmweb.pl/po/65/83/586583/7749389.3.jpg", ItemId = 1, RentDate = 0, RentName = null, Title = "Zjawa", Type = Types.DVD, UserId = 0, Year = 2015 },
+                    new Item { AuthorName = "Queen", Cover = "https://images-na.ssl-images-amazon.com/images/I/6139YfAAyBL._SS500.jpg", ItemId = 2, RentDate = 0, RentName = null, Title = "Jazz", Type = Types.CD, UserId = 0, Year = 1978 },
+                    new Item { AuthorName = "George Orwell", Cover = "https://upload.wikimedia.org/wikipedia/commons/f/fb/Animal_Farm_-_1st_edition.jpg", ItemId = 3, RentDate = 0, RentName = null, Title = "Folwark Zwierzęcy", Type = Types.Book, UserId = 0, Year = 1945 }
+                    );
+            }
+            context.SaveChanges();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using HomeLibrary.Models;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace HomeLibrary
@@ -14,12 +17,31 @@ namespace HomeLibrary
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            //CreateWebHostBuilder(args).Build().Run();
+
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<AppDbContext>();
+                    context.Database.Migrate();
+                    DbInitializer.Seed(context);
+                }
+                catch (Exception ex)
+                {
+                    // add log to file?
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
-                
+
     }
 }
