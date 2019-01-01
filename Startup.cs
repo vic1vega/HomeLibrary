d8591abbb89bd978239594323e7075d2e4d8e0da
vic1vega <janusz.pacholski@outlook.com>
2019-01-01 11:43:48 +0100

    modified AppDbcontext class (change base class to IdentityDbContext, add Identity as service to startup.cs + ef core migrations(name: AddIdentity)

--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using HomeLibrary.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -27,6 +28,7 @@ namespace HomeLibrary
         {
             services.AddDbContext<AppDbContext>
                 (options => options.UseSqlite("Data Source=HomeLibrary.db"));
+            services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
             services.AddTransient<IItemRepository, ItemRepository>();
             services.AddMvc();
         }
@@ -36,7 +38,9 @@ namespace HomeLibrary
         {
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseAuthentication();
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
